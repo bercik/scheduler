01f0eec429ea5175da2f01ce2bf8262c2d09e0e8
magda94 <magdapuscizna@gmail.com>
2016-01-04 13:26:03 +0100

    Create Window.java

---
+++ src/main/java/scheduler/Window.java
@@ -0,0 +1,45 @@
+package scheduler;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+//okienko głowne
+public class Window extends JFrame implements ActionListener{
+
+private final int sizeX=600;
+private final int sizeY=600;
+
+private JButton add;
+private JButton load;
+private JLabel info;
+private JPanel panel;
+Menu_Panel menu;
+Show_Panel show;
+	
+public Window(){
+	super("Plan zajęć");
+	super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	super.setSize(sizeX,sizeY);
+	super.setVisible(true);
+	super.setLayout(new BorderLayout());
+	super.setResizable(true);
+	
+	menu=new Menu_Panel();
+	show=new Show_Panel();
+	super.add(show,BorderLayout.WEST);
+	super.add(menu,BorderLayout.EAST);
+}
+
+public void actionPerformed(ActionEvent e){
+	
+	}
+
+}
